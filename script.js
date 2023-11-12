356f99b1a9d61400aa14738c2b9035463f3b9fe8
talafarael <127351345+talafarael@users.noreply.github.com>
2023-11-12 19:04:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-fetch('https://my-json-server.typicode.com/talafarael/aaa/db').then(data=>data.json().then(res=>console.log(res)))
+
+const bodylist=document.querySelector('.body')
+fetch('https://my-json-server.typicode.com/talafarael/aaa/db').then(data=>data.json().then(res=>bild(res.products)))
+function bild(res){console.log(res.length)
+    var html = "";
+    for (var i = 0; i < res.length; i++) {
+
+      html +=`<div><h1> ${res[i].description} </h1>
+        <img src="${res[i].photo_url}"></div>`;
+    }
+    bodylist.innerHTML = html
+
+}
